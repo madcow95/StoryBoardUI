96941dfa6f56293d7af2ea8122538dc9b58af3e4
madcow95 <lobasketve@naver.com>
2023-02-15 21:35:19 +0900

    ChatList UI생성

---
+++ ChatList/ChatList/ChatListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ChatListViewController.swift
+//  ChatList
+//
+//  Created by ChoiKwangWoo on 2023/02/15.
+//
+
+import UIKit
+
+class ChatListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
