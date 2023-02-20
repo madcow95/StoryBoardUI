d9137623b2613a963996d5d7539a703a0127197d
madcow95 <lobasketve@naver.com>
2023-02-20 21:29:54 +0900

    Paging View 최초 생성

---
+++ NRCOnboarding/NRCOnboarding/OnboardingCell.swift
@@ -0,0 +1,12 @@
+//
+//  OnboardingCell.swift
+//  NRCOnboarding
+//
+//  Created by ChoiKwangWoo on 2023/02/20.
+//
+
+import UIKit
+
+class OnboardingCell: UICollectionViewCell {
+    
+}
---
+++ NRCOnboarding/NRCOnboarding/OnboardingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  OnboardingViewController.swift
+//  NRCOnboarding
+//
+//  Created by ChoiKwangWoo on 2023/02/20.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
