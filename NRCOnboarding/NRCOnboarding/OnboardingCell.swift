4250a591e7d8a5c81187c809720147700fb00a37
madcow95 <lobasketve@naver.com>
2023-02-20 21:46:37 +0900

    UI 완성

--- NRCOnboarding/NRCOnboarding/OnboardingCell.swift
+++ NRCOnboarding/NRCOnboarding/OnboardingCell.swift
@@ -9,4 +9,13 @@ import UIKit
 
 class OnboardingCell: UICollectionViewCell {
     
+    @IBOutlet weak var thumbnailView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    func configure(_ message: OnboardingMessage) {
+        thumbnailView.image = UIImage(named: message.imageName)
+        titleLabel.text = message.title
+        descriptionLabel.text = message.description
+    }
 }
--- NRCOnboarding/NRCOnboarding/OnboardingViewController.swift
+++ NRCOnboarding/NRCOnboarding/OnboardingViewController.swift
@@ -9,7 +9,39 @@ import UIKit
 
 class OnboardingViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let message: [OnboardingMessage] = OnboardingMessage.messages
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.estimatedItemSize = .zero
+        }
     }
+}
+
+extension OnboardingViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return message.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "OnboardingCell", for: indexPath) as? OnboardingCell else {
+            return UICollectionViewCell()
+        }
+        let msg = message[indexPath.item]
+        cell.configure(msg)
+        return cell
+    }
+    
+    
+}
+
+extension OnboardingViewController: UICollectionViewDelegateFlowLayout {
+    
 }
