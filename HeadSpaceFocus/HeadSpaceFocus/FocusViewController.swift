35f8654542bebfff43d7d76ef6848627670a457b
madcow95 <lobasketve@naver.com>
2023-02-22 21:40:54 +0900

    유동적 UI Grid 최초 생성

---
+++ HeadSpaceFocus/HeadSpaceFocus/FocusViewController.swift
@@ -0,0 +1,63 @@
+//
+//  FocusViewController.swift
+//  HeadSpaceFocus
+//
+//  Created by ChoiKwangWoo on 2023/02/22.
+//
+
+import UIKit
+
+class FocusViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var items: [Focus] = Focus.list
+    var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
+    
+    typealias Item = Focus
+    enum Section {
+        case main
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Presentation, Data, Layout
+        dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
+            
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FocusCell", for: indexPath) as? FocusCell else {
+                return nil
+            }
+             cell.configure(item)
+            return cell
+        })
+        
+        // SnapShot
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(items, toSection: .main)
+        dataSource.apply(snapshot)
+        
+        collectionView.collectionViewLayout = layout()
+    }
+    
+    private func layout() -> UICollectionViewCompositionalLayout {
+        
+        // estimate: 기본적으로 입력한 값으로 설정을 하는데 변경이 될 수 있다
+        // 유동적으로 변경이 가능한가봄 최소값 느낌? 
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(50))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(50))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        // 상하좌우 Padding
+        section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20)
+        // Gruop간 Padding
+        section.interGroupSpacing = 10
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+}
