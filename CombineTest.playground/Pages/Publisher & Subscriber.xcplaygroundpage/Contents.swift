2b39259789fcc1026054743ad5f13a47437612aa
madcow95 <lobasketve@naver.com>
2023-03-01 22:09:06 +0900

    URLSession / Notification / KeyPath binding / Timer

--- CombineTest.playground/Pages/Publisher & Subscriber.xcplaygroundpage/Contents.swift
+++ CombineTest.playground/Pages/Publisher & Subscriber.xcplaygroundpage/Contents.swift
@@ -9,24 +9,24 @@ let subscription1 = just.sink { value in
     print("Received Value: \(value)")
 }
 
-let arrayPublisher = [1, 3, 5, 7, 9].publisher
-let subscription2 = arrayPublisher.sink { value in
-    print("Received Value: \(value)")
-}
-
-class MyClass {
-    var property: Int = 0 {
-        didSet {
-            print("Did set property to \(property)")
-        }
-    }
-}
-
-let object = MyClass()
-let subscription3 = arrayPublisher.assign(to: \.property, on: object)
-print("Final Value : \(object.property)")
-
-object.property = 3
+//let arrayPublisher = [1, 3, 5, 7, 9].publisher
+//let subscription2 = arrayPublisher.sink { value in
+//    print("Received Value: \(value)")
+//}
+//
+//class MyClass {
+//    var property: Int = 0 {
+//        didSet {
+//            print("Did set property to \(property)")
+//        }
+//    }
+//}
+//
+//let object = MyClass()
+//let subscription3 = arrayPublisher.assign(to: \.property, on: object)
+//print("Final Value : \(object.property)")
+//
+//object.property = 3
 
 //: [Next](@next)
 
