a23dee8ab80b65e9e4d4b64cd8a5e80b4ea2ecde
madcow95 <lobasketve@naver.com>
2023-03-01 16:02:57 +0900

    Published Test

--- CombineTest.playground/Pages/Published.xcplaygroundpage/Contents.swift
+++ CombineTest.playground/Pages/Published.xcplaygroundpage/Contents.swift
@@ -4,7 +4,22 @@ import Foundation
 import UIKit
 import Combine
 
+final class SomeViewModel {
+    @Published var name: String = "Choi"
+    var age: Int = 29
+}
 
+final class Label {
+    var text: String = ""
+}
 
+let label = Label()
+let vm = SomeViewModel()
 
+print("text : \(label.text)") // -> ""
+vm.$name.assign(to: \.text, on: label)
+print("text : \(label.text)") // -> "Choi"
+
+vm.name = "KwangWoo"
+print("text : \(label.text)") // -> "KwangWoo"
 //: [Next](@next)
