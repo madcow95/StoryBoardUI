a6f6948ac45e1c001ee01bfc1e25454d16baf2a3
madcow95 <lobasketve@naver.com>
2023-03-06 21:04:56 +0900

    Combine 사용해서 github 프로필 가져오기

--- Network in iOS.playground/Pages/Using Combine.xcplaygroundpage/Contents.swift
+++ Network in iOS.playground/Pages/Using Combine.xcplaygroundpage/Contents.swift
@@ -24,10 +24,45 @@ struct GithubProfile: Codable {
     }
 }
 
+final class NetworkService {
+    let session: URLSession
+    
+    init(configuration: URLSessionConfiguration) {
+        session = URLSession(configuration: configuration)
+    }
 
+    func fetchProfile(userName: String) -> AnyPublisher<GithubProfile, Error> {
+        let url = URL(string: "https://api.github.com/users/\(userName)")!
 
+        let publisher = session
+            .dataTaskPublisher(for: url)
+            // 서버에서 받은 response 확인 및 Data 확인
+            .tryMap { result -> Data in
+                guard let httpResponse = result.response as? HTTPURLResponse, (200..<300).contains(httpResponse.statusCode) else {
+                    let response = result.response as? HTTPURLResponse
+                    let statusCode = response?.statusCode ?? -1
+                    throw NetworkError.responseError(statusCode: statusCode)
+                }
+                return result.data
+            }
+            // 받은 Data Decoding 잘하기
+            .decode(type: GithubProfile.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+            
+        return publisher
+    }
+}
 
-
+let networkService = NetworkService(configuration: .default)
+let subscription = networkService
+    .fetchProfile(userName: "madcow95")
+    .receive(on: RunLoop.main)
+    .print("[Debug]")
+    .sink { completion in
+        print("completion >>> \(completion)")
+    } receiveValue: { profile in
+        print("profile >>> \(profile)")
+    }
 
 //: [Next](@next)
 
